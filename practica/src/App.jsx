c4d5c75a5f886ae87070d832d35e7bda82ae2365
Hans Llanos <hans.llanos@gmail.com>
2024-08-21 15:53:01 -0500

    se crea custom hooks

--- practica/src/App.jsx
+++ practica/src/App.jsx
@@ -3,8 +3,8 @@ import { Products } from './components/Products'
 import { products as initialProducts } from './mocks/products.json'
 import { Header } from './components/Header'
 
-function App() {
-  const [products] = useState(initialProducts)
+function useFilters(){
+  
   const [filters, setFilters] = useState({
     category: 'all',
     minPrice: 0
@@ -23,6 +23,13 @@ function App() {
       )
     })
   }
+
+  return { filterProducts, setFilters }
+}
+
+function App() {
+  const [products] = useState(initialProducts)
+  const {filterProducts, setFilters} = useFilters();
   const filteredProducts = filterProducts(products);
 
   return (
--- practica/src/components/Filters.jsx
+++ practica/src/components/Filters.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useId } from 'react'
 
 export function Filters({onChange}) {
     const [minPrice, setMinPrice] = useState(0);
+    const minPriceFilterId = useId();
+    const categoryFilterId = useId();
+    
     const handleChangeMinPrice = (event) => {
         setMinPrice(event.target.value);
         onChange(prevState => ({
@@ -20,18 +23,18 @@ export function Filters({onChange}) {
     return (
         <section className='filters'>
             <div>
-                <label htmlFor="price">Precio a partir de: </label>
+                <label htmlFor={minPriceFilterId}>Precio a partir de: </label>
                 <input 
                 type="range"
-                id='price'
+                id={minPriceFilterId}
                 min='0'
                 max='1000' 
                 onChange = {handleChangeMinPrice} />
                 <span>${minPrice}</span>
             </div>
             <div>
-                <label htmlFor="categoria">Categoria</label>
-                <select id="category" onChange={handleChangeCategory}>
+                <label htmlFor={categoryFilterId}>Categoria</label>
+                <select id={categoryFilterId} onChange={handleChangeCategory}>
                     <option value="all">Todas</option>
                     <option value="laptops">Portatiles</option>
                     <option value="smartphones">Celulares</option>
